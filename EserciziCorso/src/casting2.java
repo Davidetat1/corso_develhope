39ab63457531984a5df744104ed7c8469a999be7
Davidetat1 <davidetatone92@gmail.com>
2024-02-22 17:56:37 +0100

    Es4_casting2
    Esercizio tutor casting n.2

---
+++ EserciziCorso/src/casting2.java
@@ -0,0 +1,8 @@
+public class casting2 {
+    public static void main(String args[]) {
+        char a = 'a';
+        char b = 'b';
+        int sumASCII = (int)a + (int) b ;
+        System.out.println("La somma dei valori ASCII di " + a +" + "+ b + " è: " + sumASCII);
+    }
+}
