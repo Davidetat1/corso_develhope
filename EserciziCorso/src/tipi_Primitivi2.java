649292c779739490f731655ce14075e65c2cc168
Davidetat1 <davidetatone92@gmail.com>
2024-02-22 17:34:35 +0100

    Es2_TipiPrimitivi

---
+++ EserciziCorso/src/tipi_Primitivi2.java
@@ -0,0 +1,9 @@
+public class tipi_Primitivi2 {
+    public static void main(String[] args) {
+        char a ='a';
+        char b ='b';
+        String ab = "" + a+b;
+        System.out.println("il risulato di a + b è = " + ab);
+    }
+}
+
