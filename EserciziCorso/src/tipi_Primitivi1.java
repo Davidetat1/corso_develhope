7916177c05e78cc2fa7a57f161ab06eaa550ee68
Davidetat1 <davidetatone92@gmail.com>
2024-02-22 17:21:18 +0100

    Es1_TipiPrimitivi

---
+++ EserciziCorso/src/tipi_Primitivi1.java
@@ -0,0 +1,10 @@
+public class tipi_Primitivi1 {
+ public static void main(String[] args) {
+     int base = 40;
+     int altezza = 60;
+     int area = (base * altezza)/2;
+     System.out.println(" base = " + base);
+     System.out.println(" altezza = " + altezza);
+     System.out.println("il risulato dell'area del triangolo è " + area);
+  }
+}
