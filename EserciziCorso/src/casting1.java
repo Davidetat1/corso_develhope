24e6d5614f179ac7a4e25682903738a007d56d5e
Davidetat1 <davidetatone92@gmail.com>
2024-02-22 17:39:04 +0100

    Es3_Casting1

---
+++ EserciziCorso/src/casting1.java
@@ -0,0 +1,8 @@
+public class casting1 {
+    public static void main(String args[]) {
+        double a = 1.3;
+        double b = 2.6;
+        int somma = (int)a + (int)b;
+        System.out.println("somma = " + somma);
+    }
+}
