659533bd05e74e84019d1fa1994c7dc3fb335ff4
Davidetat1 <davidetatone92@gmail.com>
2024-02-22 18:44:27 +0100

    Esercizio5_Funzioni1

---
+++ EserciziCorso/src/funzioni1.java
@@ -0,0 +1,11 @@
+public class funzioni1 {
+        public static void main(String[] args) {
+            int num1 = 23;
+            int num2 = 2;
+            System.out.println("La somma di " + num1 + " e " + num2 + " è: " + sumOf(num1,num2));
+        }
+        public static int sumOf(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+
