e6d9e4b92d056d7b08a5ad0b6e7111dec7941f44
Davidetat1 <davidetatone92@gmail.com>
2024-02-23 20:26:07 +0100

    Es16_If_else2

--- EserciziCorso/src/if_Else2.java
+++ EserciziCorso/src/if_Else2.java
@@ -6,17 +6,17 @@ sulla base di questa lunghezza stampi "Lunghezza maggiore di 10",
 
 
 public class if_Else2 {
-    public static void main (String args []){
+    public static void main(String args[]) {
         String mioNome = "Davide Tatone";
         int lunghezzaStringa = mioNome.length();
 
-            if ( lunghezzaStringa < 10 ){
-                System.out.println("la stringa " + mioNome + " è lunga meno di 10 caratteri");
-            }else if ( lunghezzaStringa > 10 ){
-                System.out.println("la stringa " + mioNome + " è lunga più di 10 caratteri");
-            }else {
-                System.out.println("la stringa " + mioNome + " è uguale a 10 caratteri");
-            }
+        if (lunghezzaStringa < 10) {
+            System.out.println("la stringa " + mioNome + " è lunga meno di 10 caratteri");
+        } else if (lunghezzaStringa > 10) {
+            System.out.println("la stringa " + mioNome + " è lunga più di 10 caratteri");
+        } else {
+            System.out.println("la stringa " + mioNome + " è uguale a 10 caratteri");
+        }
 
 
     }
