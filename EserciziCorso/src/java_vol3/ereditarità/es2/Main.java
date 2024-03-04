3e3d08d7bae0cd7283789ec8ca057d952cd1b143
Davidetat1 <davidetatone92@gmail.com>
2024-03-05 00:38:05 +0100

    es2/3 ereditarietà

--- EserciziCorso/src/java_vol3/ereditarità/es2/Main.java
+++
@@ -1,9 +0,0 @@
-package java_vol3.ereditarità.es2;
-public class Main {
-    public static void main(String[] args){
-        Rettangolo rettangolo = new Rettangolo(10,3);
-        Forma forma = new Forma ();
-        forma.calcolaArea();
-        System.out.println("Area = " + rettangolo.calcolaArea());
-    }
-}
