23982cb3f030580732dfc0a6d9ff33de4508b17c
Davidetat1 <davidetatone92@gmail.com>
2024-03-17 13:11:40 +0100

    Es7_N3_HashSet

---
+++ EserciziCorso/src/java_advanced/hashset/HashSetEsercizio3.java
@@ -0,0 +1,62 @@
+/*
+
+Create la struttura, La popolate e poi implementate una ricerca nel set a partire da un valore di input,
+se lo trovate allora lo togliete dal set. Fate in modo di trovare
+tutti i valori con cui avete popolato il set in modo che rimanga vuoto.
+A quel punto stampate il set per vedere che sia effettivamente vuoto.
+
+ */
+
+
+package java_advanced.hashset;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetEsercizio3 {
+    public static void main(String[] args) {
+
+        HashSet<String> names = new HashSet<>();
+        names.add("Davide");
+        names.add("Marco");
+        names.add("Luca");
+
+        System.out.println("HashSet creato: " + names);
+
+        String valoreInput1 = "Davide";
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            String valore = iterator.next();
+            if (valore.equals(valoreInput1)) {
+                iterator.remove();
+                System.out.println("il valore \"" + valoreInput1 + "\" è stato rimosso dall'HashSet");
+            }
+        }
+
+        System.out.println("HashSet dopo la ricerca e rimozione del valore: \"" + valoreInput1 + "\" " + names);
+
+        String valoreInput2 = "Marco";
+        iterator = names.iterator();
+        while (iterator.hasNext()) {
+            String valore = iterator.next();
+            if(valore.equals(valoreInput2)){
+                iterator.remove();
+                System.out.println("il valore \"" + valoreInput2 + "\" è stato rimosso dall'HashSet");
+            }
+        }
+
+        System.out.println("HashSet dopo la ricerca e rimozione del valore: \"" + valoreInput2 + "\" " + names);
+
+        String valoreInput3 = "Luca";
+        iterator = names.iterator();
+        while(iterator.hasNext()){
+            String valore = iterator.next();
+            if(valore.equals(valoreInput3)){
+                iterator.remove();
+               System.out.println("il valore \"" + valoreInput3 + "\" è stato rimosso dall'HashSet");
+            }
+        }
+
+        System.out.println("HashSet dopo la ricerca e rimozione degli elementi:" + names);
+    }
+}
