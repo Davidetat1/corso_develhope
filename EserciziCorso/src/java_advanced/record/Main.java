efa7b786b63e7ee7ba65a748882a22ad40251a18
Davidetat1 <davidetatone92@gmail.com>
2024-03-25 01:34:25 +0100

    Es13_Record

---
+++ EserciziCorso/src/java_advanced/record/Main.java
@@ -0,0 +1,17 @@
+package java_advanced.record;
+
+public class Main {
+    public static void main(String[] args) {
+        Punto punto1 = new Punto(10 , 40);
+
+
+        System.out.println("punto x: " + punto1.x());
+        System.out.println("punto y: " + punto1.y());
+        System.out.println("punto: " + punto1);
+
+
+
+
+
+    }
+}
---
+++ EserciziCorso/src/java_advanced/record/Punto.java
@@ -0,0 +1,4 @@
+package java_advanced.record;
+
+public record Punto(int x, int y) {
+}
