b6f31d9b46ba136484e6155b4e61cc47d4aaca1a
Davidetat1 <davidetatone92@gmail.com>
2024-03-29 15:58:31 +0100

    Esercizio TestingN3

---
+++ EserciziCorso/src/java_advanced/testing/TestingEsercizio3.java
@@ -0,0 +1,30 @@
+/*
+Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+Formatta la data ottenendo 01 marzo 2023
+Stampa sulla console
+Fai attenzione a usare almeno Java 8
+Crea dei test per questo esercizio
+ */
+
+package java_advanced.testing;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestingEsercizio3 {
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println("data originale: " + data);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        String dataString = formatterDate(data, formatter);
+        System.out.println("data formattata " + dataString);
+
+    }
+
+    static String formatterDate(OffsetDateTime data, DateTimeFormatter formatter) {
+        return data.format(formatter);
+    }
+
+}
+
---
+++ EserciziCorso/src/java_advanced/testing/TestingEsercizio3Test.java
@@ -0,0 +1,23 @@
+package java_advanced.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TestingEsercizio3Test {
+
+    @Test
+    public void testFormatterDate() {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+
+        String expected = "01 marzo 2023";
+        String actual = TestingEsercizio3.formatterDate(data, formatter);
+
+        assertEquals(expected, actual);
+    }
+}
