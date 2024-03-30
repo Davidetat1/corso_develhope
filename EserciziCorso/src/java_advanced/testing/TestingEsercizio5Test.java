8b4265baedec4d81577fa9050f872bbedece28e1
Davidetat1 <davidetatone92@gmail.com>
2024-03-30 16:43:08 +0100

    Esercizio TestingN5

--- EserciziCorso/src/java_advanced/testing/TestingEsercizio5Test.java
+++ EserciziCorso/src/java_advanced/testing/TestingEsercizio5Test.java
@@ -13,13 +13,10 @@ class TestingEsercizio5Test {
     public void dataTest(){
         OffsetDateTime dateInput = OffsetDateTime.parse("2023-03-01T13:00:00Z");
 
-        // Manipolazione della data
         OffsetDateTime otherDate = dateInput.plusYears(1).plusDays(7).minusMonths(1);
 
-        // Formattazione della data in italiano
         String formattedDate = otherDate.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALIAN));
 
-        // Verifica del risultato atteso
         assertEquals("08 febbraio 2024", formattedDate);
     }
 }
